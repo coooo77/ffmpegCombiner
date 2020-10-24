02cfdda08a5477d2d09bc77c2bc5aefb6f373135
TL Chang <coooo77@hotmail.com>
2020-10-24 15:48:01 +0800

    [新增]執行合併功能

--- combine.js
+++ combine.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const { writeFile } = require('fs');
+const { exec } = require('child_process')
 
 const {
   readFilesAndSortThem,
@@ -15,5 +16,9 @@ const fileNames = readFilesAndSortThem('./files', fileNameClipper)
 listMaker('./files', fileNames)
 
 const command = cmdCommandMaker(__dirname, fileNames)
+
+writeFile(`./files/combine.bat`, command, error => console.log(error))
+
+exec('start ' + `${__dirname}` + `\\files\\combine.bat`)
 
 console.log('Done!')
--- util/helper.js
+++ util/helper.js
@@ -9,7 +9,7 @@ module.exports = {
     const fileNames = {}
     // Question:會不會有名稱排序的問題?
     fs.readdirSync(dirLocation).forEach(file => {
-      if (file.indexOf('.txt') === -1) {
+      if (file.indexOf('.mp4') !== -1) {
         const fileName = file.split(fileNameClipper)
         const name = fileName[0]
         if (!fileNames[name]) {
@@ -35,7 +35,7 @@ module.exports = {
   cmdCommandMaker(dirname, fileNames) {
     let command = `cd ${dirname}\\files\n`
     for (name in fileNames) {
-      const combinedFileName = fileNames[name][0].replace('.mp4', `_${suffix}_.mp4`)
+      const combinedFileName = fileNames[name][0].replace('.mp4', `_${suffix}.mp4`)
       const combineSetting = forceUnsafeFileNameToCombine ? '-safe 0' : ''
       command += `ffmpeg -f concat ${combineSetting} -i ${name}.txt -c copy ${combinedFileName}\n`
     }
