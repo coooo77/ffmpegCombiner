3250830ce561fd6c186b635b02c6fd58d25ae53f
TL Chang <coooo77@hotmail.com>
2020-10-24 11:37:50 +0800

    [優化]將功能獨立到helper中

--- combine.js
+++ combine.js
@@ -1,31 +1,17 @@
 const fs = require('fs');
+
+const {
+  readFilesAndSortThem,
+  listMaker
+} = require('./util/helper')
+
 const {
   dirLocation,
   fileNameClipper
 } = require('./config/config')
 
-const fileNames = {}
-// Question:會不會有名稱排序的問題?
-fs.readdirSync(dirLocation).forEach(file => {
-  if (file.indexOf('.txt') === -1) {
-    const fileName = file.split(fileNameClipper)
-    const name = fileName[0]
-    if (!fileNames[name]) {
-      fileNames[name] = [file]
-    } else {
-      fileNames[name].push(file)
-    }
-  }
-});
+const fileNames = readFilesAndSortThem(dirLocation, fileNameClipper)
 
-//開始製作合併需要的list
-for (userName in fileNames) {
-  let text = ''
-  for (files of fileNames[userName]) {    
-    text += `file '${files}'\n`
-  }
-  fs.writeFileSync(`${dirLocation}/${userName}.txt`, text)
-}
-
+listMaker(dirLocation,fileNames)
 
 console.log('Done!')
--- util/helper.js
+++ util/helper.js
@@ -1,1 +1,31 @@
-module.exports = {}
+const fs = require('fs');
+
+module.exports = {
+  readFilesAndSortThem(dirLocation, fileNameClipper) {
+    const fileNames = {}
+    // Question:會不會有名稱排序的問題?
+    fs.readdirSync(dirLocation).forEach(file => {
+      if (file.indexOf('.txt') === -1) {
+        const fileName = file.split(fileNameClipper)
+        const name = fileName[0]
+        if (!fileNames[name]) {
+          fileNames[name] = [file]
+        } else {
+          fileNames[name].push(file)
+        }
+      }
+    });
+
+    return fileNames
+  },
+  listMaker(dirLocation, fileNames) {
+    //開始製作合併需要的list
+    for (userName in fileNames) {
+      let text = ''
+      for (files of fileNames[userName]) {
+        text += `file '${files}'\n`
+      }
+      fs.writeFileSync(`${dirLocation}/${userName}.txt`, text)
+    }
+  }
+}
